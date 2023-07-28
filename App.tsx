85a0ce0692d95958b22842d0f8def1922334da34
soonyoong@gmail.com <soonyoong@gmail.com>
2023-07-28 23:00:50 +0800

    Added react native paper

--- App.tsx
+++ App.tsx
@@ -2,15 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './navigators/RootNavigator';
+import {PaperProvider} from "react-native-paper";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style='dark' />
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <StatusBar style='dark' />
+        <NavigationContainer>
+          <RootNavigator />
+        </NavigationContainer>
+      </View>
+    </PaperProvider>
   );
 }
 
