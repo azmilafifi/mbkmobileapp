cd65c7af46048e7ec057087b98bd37b0a626c939
Azmil <azmilafifi@yahoo.com>
2023-08-09 03:24:23 +0800

    added validation

--- src/components/Login/LoginBottomSheetContent.tsx
+++ src/components/Login/LoginBottomSheetContent.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { View, TextInput, TouchableOpacity, StyleSheet, Text } from 'react-native';
-import {AuthControllerService} from "../../openapi/codegen";
-
+import { AuthControllerService } from "../../openapi/codegen";
 
 interface LoginBottomSheetContentProps {
   onClose: () => void;
@@ -11,35 +10,60 @@ interface LoginBottomSheetContentProps {
 const LoginBottomSheetContent: React.FC<LoginBottomSheetContentProps> = ({ onClose, onLoginSuccess }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
   const handleLogin = () => {
-    // Implement your login logic here (e.g., API call)
-    // For this example, let's assume login is successful
-    AuthControllerService.authenticateUserUsingPost({loginRequest: {email: username, password}}).then((data)=> {
-      onLoginSuccess();
-      // Close the bottom sheet after successful login
-      onClose();
-      console.log(data.accessToken);
-    }).catch((err)=> {
-      console.log('Error: ' + err);
-    })
+    // Validation checks
+    if (!username || !password) {
+      setError('Please fill in all fields');
+      return;
+    }
+
+    if (!isValidEmail(username)) {
+      setError('Please enter a valid email address');
+      return;
+    }
+
+    // Clear previous errors
+    setError('');
+
+    // API call for authentication
+    AuthControllerService.authenticateUserUsingPost({ loginRequest: { email: username, password } })
+      .then((data) => {
+        onLoginSuccess();
+        // Close the bottom sheet after successful login
+        onClose();
+        console.log(data.accessToken);
+      })
+      .catch((err) => {
+        console.log('Error: ' + err);
+        setError('Login failed. Please try again.');
+      });
+  };
+
+  const isValidEmail = (email: string) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
   };
 
   return (
     <View style={styles.bottomSheetContent}>
       <TextInput
-        style={styles.input}
+        style={[styles.input, error ? styles.inputError : null]} // Adjusted style condition
         placeholder="Email"
         value={username}
         onChangeText={setUsername}
       />
       <TextInput
-        style={styles.input}
+        style={[styles.input, error ? styles.inputError : null]} // Adjusted style condition
         placeholder="Password"
         secureTextEntry
         value={password}
         onChangeText={setPassword}
       />
+      <View style={styles.errorContainer}>
+        {error ? <Text style={styles.errorText}>{error}</Text> : null}
+      </View>
       <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
         <Text style={styles.loginButtonText}>Login</Text>
       </TouchableOpacity>
@@ -51,6 +75,7 @@ const styles = StyleSheet.create({
   bottomSheetContent: {
     padding: 20,
     alignItems: 'center',
+    paddingTop:50
   },
   input: {
     width: '100%',
@@ -62,6 +87,14 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     color: '#878787',
     backgroundColor: 'white',
+  },
+  inputError: {
+    borderColor: 'red',
+  },
+  errorText: {
+    color: 'red',
+    marginBottom: 15,
+    textAlign: 'center',
   },
   loginButton: {
     backgroundColor: '#EEEEEE',
@@ -75,6 +108,11 @@ const styles = StyleSheet.create({
     color: '#878787',
     fontSize: 18,
     textAlign: 'center',
+  },
+  errorContainer: {
+    minHeight: 20,
+    marginBottom: 15,
+    width: '100%',
   },
 });
 
--- src/screens/Login/LoginScreen.tsx
+++ src/screens/Login/LoginScreen.tsx
@@ -4,6 +4,7 @@ import React, { useRef } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, ImageBackground } from 'react-native';
 import RBSheet, { RBSheetProps } from 'react-native-raw-bottom-sheet';
 import LoginBottomSheetContent from '../../components/Login/LoginBottomSheetContent';
+import SignUpScreen from '../Signup/SingUpScreen';
 
 interface LoginScreenProps {
   onLoginSuccess: () => void;
@@ -14,12 +15,15 @@ const PIC_url = 'https://images.unsplash.com/photo-1584870831686-c2637ecbc95d?ix
 
 const LoginScreen: React.FC<LoginScreenProps> = ({ onLoginSuccess, onSignUpPress }) => {
   const bottomSheetRef = useRef<RBSheet>(null);
-
+  const bottomSheetSignUp = useRef<RBSheet>(null);
   const handleLogin = () => {
     // Open the bottom sheet when the "Login" button is pressed
     bottomSheetRef.current?.open();
   };
 
+  const handleSignUp = () => {
+    bottomSheetSignUp.current?.open();
+  }
   return (
     <View style={styles.container}>
       <ImageBackground source={{ uri: PIC_url }} style={styles.backgroundImage}>
@@ -27,22 +31,45 @@ const LoginScreen: React.FC<LoginScreenProps> = ({ onLoginSuccess, onSignUpPress
           <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
             <Text style={styles.loginButtonText}>Login</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.signUpButton} onPress={onSignUpPress}>
+          <TouchableOpacity style={styles.signUpButton} onPress={handleSignUp}>
             <Text style={styles.signupButtonText}>Sign Up</Text>
           </TouchableOpacity>
         </View>
       </ImageBackground>
       <RBSheet
         ref={bottomSheetRef}
-        height={300}
+        height={400}
         duration={250}
         closeOnDragDown={true}
         closeOnPressMask={true}
+        customStyles={{
+          container: {
+            borderTopLeftRadius: 40, // Add the desired border radius values
+            borderTopRightRadius: 40,
+          },
+        }}
       >
         <LoginBottomSheetContent
           onLoginSuccess={onLoginSuccess}
           onClose={() => bottomSheetRef.current?.close()}
         />
+      </RBSheet>
+      <RBSheet
+        ref={bottomSheetSignUp}
+        height={700}
+        duration={250}
+        closeOnDragDown={true}
+        closeOnPressMask={true}
+        customStyles={{
+          container: {
+            borderTopLeftRadius: 40, // Add the desired border radius values
+            borderTopRightRadius: 40,
+          },
+        }}
+      >
+        <SignUpScreen
+          onSignUpSuccess={onSignUpPress}
+          />
       </RBSheet>
     </View>
   );
@@ -91,6 +118,9 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     fontWeight: '500',
+  },
+  bottomSheet: {
+    borderRadius: 20,
   },
 });
 
